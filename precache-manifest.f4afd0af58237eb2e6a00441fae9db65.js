5e984df1020857a6f1440c12a4c74e912bae4e67
sibashish biswas <sibashishbiswas349@gmail.com>
2020-07-24 12:14:04 +0530

    Updates

---
+++ precache-manifest.f4afd0af58237eb2e6a00441fae9db65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deff34495a7dcc6d1dd7c849d729cbf8",
+    "url": "/React_MusicApp/index.html"
+  },
+  {
+    "revision": "3a5248ef3606dc7f7c02",
+    "url": "/React_MusicApp/static/css/main.013c85b9.chunk.css"
+  },
+  {
+    "revision": "748f4a902893a04b1f49",
+    "url": "/React_MusicApp/static/js/2.ed4d46da.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React_MusicApp/static/js/2.ed4d46da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5248ef3606dc7f7c02",
+    "url": "/React_MusicApp/static/js/main.e4a7560e.chunk.js"
+  },
+  {
+    "revision": "65cabc3ca0b706218579",
+    "url": "/React_MusicApp/static/js/runtime-main.641a30e5.js"
+  }
+]);
